f7910f2c45f06781e94efef21f9c89824cecc0ca
seanarch <zhuqingyun@outlook.com>
2023-06-24 17:08:05 -0600

    Add root route

--- estoreServer/index.js
+++ estoreServer/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  let prodData = {
+    pName: "Jackets",
+    price: 45,
+    img: "shop-1.jpg",
+  };
+
+  res.status(200).send(prodData);
+});
+
 const PORT = 5001;
 
 const server = app.listen(PORT, () => {
