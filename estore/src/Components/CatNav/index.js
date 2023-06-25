9c16982d54e9a56b71a354962383825f41585695
seanarch <zhuqingyun@outlook.com>
2023-06-25 12:18:15 -0600

    Config reducer and action for category nav

--- estore/src/Components/CatNav/index.js
+++ estore/src/Components/CatNav/index.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./_cat-nav.scss";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import categorySlice from "../../Redux/Category/categorySlice";
+import { getCategories } from "../../Redux/Category/actions";
 
 const CatNav = () => {
-  const categories = useSelector(categorySlice.getInitialState);
+  const categories = useSelector((state) => state.categoryReducer.categories);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, []);
+
+  console.log(categories);
+
   return (
     <div className="cat-nav-container container">
       <ul>
