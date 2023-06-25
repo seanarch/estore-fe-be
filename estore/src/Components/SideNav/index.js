48efa9c128de2b993e11fd875b8b4de256191079
seanarch <zhuqingyun@outlook.com>
2023-06-25 16:03:13 -0600

    Fetch categories for accordion

--- estore/src/Components/SideNav/index.js
+++ estore/src/Components/SideNav/index.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./_side-nav.scss";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import accordionSlice from "../../Redux/Accordion/accordionSlice";
+import { getCategories } from "../../Redux/Category/actions";
 
 const SideNav = () => {
-  const accordionData = useSelector(accordionSlice.getInitialState);
+  const accordionData = useSelector(
+    (state) => state.categoryReducer.categories
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, []);
+
   return (
     <div className="side-nav">
       <div className="section-title">
@@ -12,38 +21,45 @@ const SideNav = () => {
       </div>
       <div className="accordion">
         {accordionData.map((accordionCategory, key) => {
-          return (
-            <div className="accordion-item individual-category">
-              <div className="accordion-header">
-                <button
-                  className="accordion-button"
-                  data-bs-target={"#collapse" + key}
-                  data-bs-toggle="collapse"
-                >
-                  <div className="category-title">
-                    <a href="#">{accordionCategory.category}</a>
-                  </div>
-                </button>
-              </div>
+          if (accordionCategory.parent_category_id === null) {
+            return (
+              <div className="accordion-item individual-category">
+                <div className="accordion-header">
+                  <button
+                    className="accordion-button"
+                    data-bs-target={"#collapse" + key}
+                    data-bs-toggle="collapse"
+                  >
+                    <div className="category-title">
+                      <a href="#">{accordionCategory.category}</a>
+                    </div>
+                  </button>
+                </div>
 
-              <div
-                className="accordion-collapse collapse show"
-                id={"collapse" + key}
-              >
-                <div className="accordion-body">
-                  <ul>
-                    {accordionCategory.items.map((item) => {
-                      return (
-                        <li className="sub-items">
-                          <a href="#">{item}</a>
-                        </li>
-                      );
-                    })}
-                  </ul>
+                <div
+                  className="accordion-collapse collapse show"
+                  id={"collapse" + key}
+                >
+                  <div className="accordion-body">
+                    <ul>
+                      {accordionData.map((subCategory) => {
+                        if (
+                          accordionCategory.id ===
+                          subCategory.parent_category_id
+                        ) {
+                          return (
+                            <li className="sub-items">
+                              <a href="#">{subCategory.category}</a>
+                            </li>
+                          );
+                        }
+                      })}
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
+            );
+          }
         })}
       </div>
     </div>
