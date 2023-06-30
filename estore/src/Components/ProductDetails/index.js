8fcce63d52dc8b40ca263a5d6e792812197cb4da
seanarch <zhuqingyun@outlook.com>
2023-06-30 11:48:59 -0600

    Styling product detail

--- estore/src/Components/ProductDetails/index.js
+++ estore/src/Components/ProductDetails/index.js
@@ -1,42 +1,49 @@
-import React from "react";
 import { useLocation } from "react-router-dom";
+import "./_product-detail.scss";
 
 const ProductDetails = () => {
   const location = useLocation();
+
   console.log(location);
+
   return (
     <div>
-      <div>
-        <div>
+      <div className="row conatiner my-5 product-details-container">
+        <div className="col-5 product-img-container">
           <img
             src={require("../../assets/images/shop/" +
               location.state.product_img)}
             alt=""
           />
         </div>
-        <div>
-          <span>{location.state.product_name}</span>
-          <div>
+        <div className="col-7 product-info">
+          <span id="product-name"> {location.state.product_name} </span>
+
+          <div className="rating-container">
             <i className="fa fa-star" />
             <i className="fa fa-star" />
             <i className="fa fa-star" />
             <i className="fa fa-star" />
             <i className="fa fa-star" />
           </div>
-          <div>
-            MSRP : <span>{location.state.price}</span>
-            <div>Inclusive of all taxes. </div>
+          <hr />
+          <div className="product-price">
+            MSRP : <span className="price"> ${location.state.price} </span>
+            <div style={{ fontSize: "0.8em" }}> Inclusive of all taxes. </div>
           </div>
-          <div>
-            <span>Some Product Desc</span>
+          <div className="my-3 product-description">
+            <span> Some Product Description Given Here... </span>
           </div>
-          <div>
-            <div>
-              <div>
+
+          <div className="my-5">
+            <div className="btn btn-warning cart-button">
+              <div className="cart-icon-container">
                 <i className="fa fa-shopping-cart" />
               </div>
+              <div className="cart-text-container">
+                <p> Add to Cart </p>
+              </div>
             </div>
-            <p>Add to Cart</p>
           </div>
         </div>
       </div>
