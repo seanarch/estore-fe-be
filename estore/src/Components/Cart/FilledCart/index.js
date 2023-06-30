6e4b54b6e9d1e41b7fdad614886d355032c5ca89
seanarch <zhuqingyun@outlook.com>
2023-06-30 14:01:00 -0600

    Fetch cart data, show selected products, cart summary and styling

--- estore/src/Components/Cart/FilledCart/index.js
+++ estore/src/Components/Cart/FilledCart/index.js
@@ -1,7 +1,75 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const FilledCart = () => {
-  return <div>FilledCart</div>;
+  const cart = useSelector((state) => state.cr);
+
+  return (
+    <div>
+      <div className="row my-5">
+        <div className="col-8 p-4">
+          {cart.cartItems.map((item) => {
+            return (
+              <div>
+                <div className="row">
+                  <div className="col-4">
+                    <img
+                      src={require("../../../assets/images/shop/" +
+                        item.product_img)}
+                      alt=""
+                    />
+                  </div>
+                  <div className="col-8">
+                    <div className="p-3">
+                      <span>{item.product_name}</span>
+                      <div>
+                        <span>${item.price}</span>
+                      </div>
+                      <div>
+                        <i className="fa fa-star text-warning" />
+                        <i className="fa fa-star text-warning" />
+                        <i className="fa fa-star text-warning" />
+                        <i className="fa fa-star text-warning" />
+                        <i className="fa fa-star text-warning" />
+                      </div>
+                      <hr />
+                      <div>
+                        <div className="btn-group mx-3">
+                          <div className="btn btn-outline-dark">
+                            <span> - </span>
+                          </div>
+                          <div className="btn"> 0 </div>
+                          <div className="btn btn-outline-dark">
+                            <span> + </span>
+                          </div>
+                        </div>
+                        <div className="btn btn-outline-danger mx-4">
+                          <span>
+                            <i className="fa fa-trash" mx-2 /> Remove Item
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="col-4 px-4 py-3 my-4">
+          <h2 className="mb-5 mt-3">Summary</h2>
+          <div>
+            <span> Cart Total : ${cart.totalItemsPrice}</span>
+            <span> Shipping Charges : Free</span>
+            <hr />
+            <span> Total : ${cart.totalItemsPrice}</span>
+            <hr />
+          </div>
+          <div className="btn btn-outline-dark w-100 mb-4 mt-1">Checkout</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FilledCart;
