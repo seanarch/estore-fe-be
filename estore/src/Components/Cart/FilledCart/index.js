14b2c0b48f5ac7ef940d4b2e57ad5ccacda93c03
seanarch <zhuqingyun@outlook.com>
2023-06-30 14:27:57 -0600

    Update filledcart styling

--- estore/src/Components/Cart/FilledCart/index.js
+++ estore/src/Components/Cart/FilledCart/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import "./_filled-cart.scss";
 
 const FilledCart = () => {
   const cart = useSelector((state) => state.cr);
 
   return (
     <div>
-      <div className="row my-5">
+      <div className="row my-5 fc-main-div">
         <div className="col-8 p-4">
           {cart.cartItems.map((item) => {
             return (
               <div>
-                <div className="row">
+                <div className="row cart-item-card">
                   <div className="col-4">
                     <img
                       src={require("../../../assets/images/shop/" +
@@ -20,9 +21,11 @@ const FilledCart = () => {
                     />
                   </div>
                   <div className="col-8">
-                    <div className="p-3">
-                      <span>{item.product_name}</span>
-                      <div>
+                    <div className="p-3 cart-item-details">
+                      <span className="cart-item-name">
+                        {item.product_name}
+                      </span>
+                      <div className="cart-item-price">
                         <span>${item.price}</span>
                       </div>
                       <div>
@@ -33,7 +36,7 @@ const FilledCart = () => {
                         <i className="fa fa-star text-warning" />
                       </div>
                       <hr />
-                      <div>
+                      <div className="cart-edit-container">
                         <div className="btn-group mx-3">
                           <div className="btn btn-outline-dark">
                             <span> - </span>
@@ -52,17 +55,20 @@ const FilledCart = () => {
                     </div>
                   </div>
                 </div>
+                <hr />
               </div>
             );
           })}
         </div>
-        <div className="col-4 px-4 py-3 my-4">
+        <div className="col-4 px-4 py-3 my-4 cart-summary">
           <h2 className="mb-5 mt-3">Summary</h2>
           <div>
             <span> Cart Total : ${cart.totalItemsPrice}</span>
             <span> Shipping Charges : Free</span>
             <hr />
-            <span> Total : ${cart.totalItemsPrice}</span>
+            <span className="summary-total">
+              Total : ${cart.totalItemsPrice}
+            </span>
             <hr />
           </div>
           <div className="btn btn-outline-dark w-100 mb-4 mt-1">Checkout</div>
