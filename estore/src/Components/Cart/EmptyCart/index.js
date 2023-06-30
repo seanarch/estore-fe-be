c433718eb04953c2936048806e5cb65243af509e
seanarch <zhuqingyun@outlook.com>
2023-06-30 12:20:35 -0600

    Update empty cart component

--- estore/src/Components/Cart/EmptyCart/index.js
+++ estore/src/Components/Cart/EmptyCart/index.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./_empty-cart.scss";
 
 const EmptyCart = () => {
-  return <div>EmptyCart</div>;
+  return (
+    <div className="p-4 ec-main-div">
+      <span className="my-5 ec-text">The Cart Is Empty</span>
+      <hr />
+      <Link to="/">
+        <div className="btn btn-warning my-3">
+          <span style={{ fontWeight: 500 }}>Continue Shopping</span>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default EmptyCart;
